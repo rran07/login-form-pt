9c23fb37a3b27330bded1acd036190b91081a669
Raniel Ronidel <r.ronidel0617@gmail.com>
2024-09-02 22:47:41 +0800

    Refractoring the route files - make it shorter

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('dashboard');
-});
-Route::get('/team', function () {
-    return view('team');
-});
+Route::get('/', 'dashboard');
+Route::get('/team', 'team');
+Route::get('/reports', 'reports');
+Route::get('/calendar', 'calendar');
+Route::get('/projects', 'projects');
 
-Route::get('/reports', function () {
-    return view('reports');
-});
-
-Route::get('/calendar', function () {
-    return view('calendar');
-});
-
-Route::get('/projects', function () {
-    return view('projects');
-});
 
 Route::view('/register', 'register');
