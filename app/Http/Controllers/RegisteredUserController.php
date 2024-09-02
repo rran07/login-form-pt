9e3d1b7cd72c6e87c9aa7caa7182e9523d919bf6
Raniel Ronidel <r.ronidel0617@gmail.com>
2024-09-03 00:40:51 +0800

    Storing the registered data

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class RegisteredUserController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('auth.register');
+    }
+
+    public function store()
+    {
+        $validatedAttributes = request()->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', Password::min(6), 'confirmed'],
+        ]);
+
+        $user = User::create($validatedAttributes);
+
+        Auth::login($user);
+
+        return redirect('/');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,20 @@
 <?php
 
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionUserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'dashboard');
-Route::get('/team', 'team');
-Route::get('/reports', 'reports');
-Route::get('/calendar', 'calendar');
-Route::get('/projects', 'projects');
+Route::view('/', 'dashboard');
+Route::view('/team', 'team');
+Route::view('/projects', 'projects');
+Route::view('/calendar', 'calendar');
+Route::view('/reports', 'reports');
 
 
-Route::view('/register', 'register');
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+
+Route::get('/login', [SessionUserController::class, 'create']);
+Route::post('/login', [SessionUserController::class, 'store']);
+
